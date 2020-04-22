076f1a889b908be702162dba7ec24382193ee373
Gigi Minova <minova.gigi@gmail.com>
2020-04-22 20:50:06 +0100

    Add styled components

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../logo.svg";
+import * as S from "./App.styled";
+
+export const App = () => (
+  <S.Container>
+    <S.Logo src={logo} alt="logo" />
+    <p>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+    <S.Link
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </S.Link>
+  </S.Container>
+);
