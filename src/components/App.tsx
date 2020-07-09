2f828d6e523d13668bea8bbc5d166f9a1127d055
Gigi Minova <minova.gigi@gmail.com>
2020-07-09 20:16:51 +0100

    add text field component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import logo from './svgs/logo.svg';
 import * as S from './App.styled';
+import { TextField } from './TextField';
 
 export const App: React.FC = () => (
   <S.Container>
-    <S.Logo src={logo} alt="logo" />
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
-    <S.Link
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </S.Link>
+    <TextField text="Some text" />
   </S.Container>
 );
---
+++ src/components/TextField.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface Props {
+  text: string;
+}
+export const TextField: React.FC<Props> = () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
