1a7712525f243f68cc9580acb517a48c2c30c958
tetsuyauno <tetsuya.uno1@gmail.com>
2020-07-09 21:00:00 +0100

    add state for textfield input

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   text: string;
 }
 export const TextField: React.FC<Props> = () => {
+  const [input, setInput] = useState('');
+
   return (
     <div>
-      <input />
+      <h1>Enter your name</h1>
+      <input onChange={e => setInput(e.target.value)} />
+      <h2>{input}</h2>
     </div>
   );
 };
