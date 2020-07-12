199c5042b76d5cbe42ce302c805bf7cfadf2482d
Gigi Minova <minova.gigi@gmail.com>
2020-07-12 21:14:41 +0100

    add pokemon to vocabulary page

--- src/components/Vocabulary.tsx
+++ src/components/Vocabulary.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import { Dashboard } from './Dashboard';
+import { generateRandomInteger } from '../utils/functions';
 
-export const Vocabulary = () => <Dashboard />;
+export const Vocabulary = () => {
+  return (
+    <>
+      <Dashboard />
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${generateRandomInteger(
+          1,
+          807
+        )}.png`}
+        alt="pokemon"
+      />
+    </>
+  );
+};
