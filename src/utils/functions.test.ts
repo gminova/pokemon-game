12c3c7caaaaf978d9df7bd0fa5bb04263231d04b
tetsuyauno <tetsuya.uno1@gmail.com>
2020-07-14 20:37:29 +0100

    added function to get/update score

--- src/utils/functions.test.ts
+++ src/utils/functions.test.ts
@@ -1,4 +1,9 @@
-import { generateRandomInteger } from './functions';
+import {
+  generateRandomInteger,
+  increaseScore,
+  getScore,
+  setScore,
+} from './functions';
 
 describe('generateRandomInteger util', () => {
   const min = 1;
@@ -7,5 +12,42 @@ describe('generateRandomInteger util', () => {
   it('generates a random integer between 1 and 807', () => {
     expect(generateRandomInteger(min, max)).toBeGreaterThanOrEqual(min);
     expect(generateRandomInteger(min, max)).toBeLessThanOrEqual(max);
+  });
+});
+
+describe('increaseScore util', () => {
+  it('increases score by 1', () => {
+    expect(increaseScore(0)).toEqual(1);
+  });
+});
+
+describe('getScore util', () => {
+  const dummyUser = JSON.stringify({
+    userName: 'Ash',
+    score: 5,
+    userPokemons: [],
+  });
+
+  it('retrieves score from local storage', () => {
+    window.localStorage.setItem('pokemonUser', dummyUser);
+    expect(getScore()).toEqual(5);
+    window.localStorage.removeItem('pokemonUser');
+  });
+});
+
+describe('setScore util', () => {
+  const dummyUser = JSON.stringify({
+    userName: 'Ash',
+    score: 5,
+    userPokemons: [],
+  });
+
+  it('sets score in local storage', () => {
+    window.localStorage.setItem('pokemonUser', dummyUser);
+    setScore(10);
+    expect(
+      JSON.parse(window.localStorage.getItem('pokemonUser') || '').score
+    ).toEqual(10);
+    window.localStorage.removeItem('pokemonUser');
   });
 });
